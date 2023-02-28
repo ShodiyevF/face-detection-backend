b0468d321021323e6600d391fba619ac64ce8ed4
Fayzulloh <71442024+ShodiyevF@users.noreply.github.com>
2023-02-28 17:57:53 +0500

    fix jwt file

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -2,10 +2,18 @@ require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'development'}.
 const jwt = require('jsonwebtoken')
 
 async function createToken(id) {
-    const token = await jwt.sign({exp: +process.env.JWT_EXPIRATION, id: id}, process.env.JWT_SECRET)
+    const token = await jwt.sign({id: id}, process.env.JWT_SECRET, {
+        expiresIn: +process.env.JWT_EXPIRATION
+    })
     return token
 }
 
+async function tokenVerifer(token) {
+    const verifed = await jwt.verify(token, process.env.JWT_SECRET)
+    return verifed
+}
+
 module.exports = {
-    createToken
+    createToken,
+    tokenVerifer
 }
