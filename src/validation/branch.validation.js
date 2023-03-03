4ccae807dde09f5284c7461cab6bbb6a134a1454
BahodirDev <mydatabases714@gmail.com>
2023-03-03 17:33:07 +0500

    fix branches status

--- src/validation/branch.validation.js
+++ src/validation/branch.validation.js
@@ -3,13 +3,13 @@ const { error } = require('../config/error.names');
 function createBranchValidation(req, res, next) {
     const body = req.body;
     if (!body.branch_name) {
-        res.json({
+        res.status(404).json({
             status: 404,
             error: error.VALIDATION_ERROR,
             message: 'branch_name majburiy',
         });
     } else if (body.branch_name.length > 30) {
-        res.json({
+        res.status(404).json({
             status: 404,
             error: error.VALIDATION_ERROR,
             message: `branch_name uzunligi 30tadan kichik bo'lishi kerak`,
@@ -24,13 +24,13 @@ function updateBranchValidation(req, res, next) {
     const params = req.params;
 
     if (body.branch_name && body.branch_name.length > 30) {
-        res.json({
+        res.status(404).json({
             status: 404,
             error: error.VALIDATION_ERROR,
             message: `branch_name uzunligi 30tadan kichik bo'lishi kerak`,
         });
     } else if (!params.branch_id.match('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')) {
-        res.json({
+        res.status(404).json({
             status: 404,
             error: error.VALIDATION_ERROR,
             message: `Filial uuid bo'lishi kerek`,
@@ -43,7 +43,7 @@ function updateBranchValidation(req, res, next) {
 function deleteBranchValidation(req, res, next) {
     const params = req.params;
     if (!params.branch_id.match('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')) {
-        res.json({
+        res.status(404).json({
             status: 404,
             error: error.VALIDATION_ERROR,
             message: `Filial uuid bo'lishi kerek`,
