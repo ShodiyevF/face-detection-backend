518bbedc7f7daf5047c08713800b8c00ce818643
masterdegreeineurope <masterdegreeineurope@gmail.com>
2023-03-03 19:18:54 +0500

    Added Crud

--- src/modules/user-role/user.role.model.js
+++ src/modules/user-role/user.role.model.js
@@ -2,23 +2,87 @@ const { error } = require('../../config/error.names');
 const { uniqRow } = require('../../lib/pg');
 
 async function createUserRoleModel(body) {
-    const userRole = await uniqRow('select * from userrole where role_name = $1', body.role_name);
+    const userRole = await uniqRow('select * from userrole where role_name = ($1)', body.role_name);
+    console.log(userRole);
     if (userRole.rows.length) {
         return {
             action: true,
             status: 400,
             error: error.USERROLE_ALREADY_EXISTS,
-            message: 'userrole already exists',
+            message: 'Bu kasb allaqachon mavjud',
         };
     }
     await uniqRow(`insert into userrole(role_name) values($1)`, body.role_name);
+
     const role = await uniqRow('select * from userrole where role_name = $1', body.role_name);
+
     return role.rows;
 }
 
 async function getUserRoleModel() {
     const userRole = await uniqRow('select * from userrole;');
+
     return userRole.rows;
 }
 
-module.exports = { createUserRoleModel, getUserRoleModel };
+async function updateUserRoleModel(body, params) {
+
+    const userRole = await uniqRow(
+        'select * from userrole where role_id = $1, params.role_id');
+
+    if (!userRole.rows.length) {
+        return {
+            action: true,
+            status: 404,
+            error: error.USERROLE_NOT_FOUND,
+            message: `${params.role_id} bunday kasb topilmadi !`,
+        };
+    }
+
+    const roleName = await uniqRow('select * from userrole where role_name = $1, body.role_name');
+
+    if (!roleName.rows.length) {
+        return {
+            action: true,
+            status: 404,
+            error: error.USERROLE_ALREADY_EXISTS,
+            message: 'bunday kasb nomi mavjud',
+        };
+    }
+
+    const result = await uniqRow(
+        'update userrole set role_name = $1 where role_id = $2 returning *', role_name, role_id);
+
+    return result.rows;
+}
+
+async function deleteUserRoleModel(params) {
+    const userRole = await uniqRow(
+        'select * from userrole where role_id = $1', params.role_id);
+
+    if (!userRole.rows.length) {
+        return {
+            action: true,
+            status: 404,
+            error: error.USERROLE_NOT_FOUND,
+            message: `${params.role_id} bunday kasb topilmadi !`,
+        };
+    }
+
+    const selecteduser = await uniqRow(`select * from users where role_id = $1`, params.role_id);
+
+    if (selecteduser.rows.length) {
+        return {
+            action: true,
+            status: 400,
+            error: error.USERROLE_REFERENCES_WITH_USER,
+            message: `${params.role_id} bunday kasb qaysidir foydalanuvchiga ulangan !`,
+        };
+    }
+
+    const result = await uniqRow('delete from userrole where role_id = ($1) returning *', params.role_id);
+
+    return result.rows;
+}
+
+module.exports = { createUserRoleModel, getUserRoleModel, updateUserRoleModel, deleteUserRoleModel };
---
+++ src/validation/user.role.validation.js
@@ -0,0 +1,38 @@
+const { error } = require('../config/error.names');
+
+function userRoleValidation(req, res, next) {
+    const body = req.body;
+
+    console.log(body);
+    if (!body.role_name) {
+        res.json({
+            status: 404,
+            error: error.VALIDATION_ERROR,
+            message: 'role_name kiritish majburiy',
+        });
+    } else if (body.role_name.length > 64) {
+        res.json({
+            status: 404,
+            error: error.VALIDATION_ERROR,
+            message: `userRole name uzunligi 64 dan kichik bo'lishi kerak`,
+        });
+    } else {
+        next();
+    }
+}
+
+function deleteUserRoleValidation(req, res, next) {
+    const params = req.params;
+    console.log(params);
+    if (!params.role_id.match('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')) {
+        res.json({
+            status: 404,
+            error: error.VALIDATION_ERROR,
+            message: `kasb uuid bo'lishi kerek`
+        });
+    } else {
+        next();
+    }
+}
+
+module.exports = { userRoleValidation, deleteUserRoleValidation };
